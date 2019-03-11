831c493951ae0e242e6a7e4e2455b6c455bbfbe8
Audrey Walters <walterae@rose-hulman.edu>
2019-03-11 00:44:08 -0400

    Done.

--- src/m9_using_objects.py
+++ src/m9_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Audrey Walters.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,6 +14,9 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
+    two_circles()
+    circle_and_rectangle()
+    lines()
 
 
 def two_circles():
@@ -27,14 +30,20 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its green doc-string above.
+    # DONE: 2. Implement this function, per its green doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.pdf  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # -------------------------------------------------------------------------
-
-
+    window = rg.RoseWindow()
+    circle1 = rg.Circle(rg.Point(200,200),10)
+    circle1.fill_color = 'red'
+    circle2 = rg.Circle(rg.Point(300,100),60)
+    circle1.attach_to(window)
+    circle2.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
 def circle_and_rectangle():
     """
     -- Constructs an rg.RoseWindow.
@@ -67,7 +76,7 @@ def circle_and_rectangle():
            150.0
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its green doc-string above.
+    # DONE: 3. Implement this function, per its green doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -75,8 +84,30 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # -------------------------------------------------------------------------
-
-
+    window = rg.RoseWindow()
+    rectangle = rg.Rectangle(rg.Point(100,100),rg.Point(200,300))
+    rectangle.outline_thickness = 1
+    rectangle.fill_color = 'red'
+    circle = rg.Circle(rg.Point(350,200),10)
+    circle.outline_thickness = 2
+    circle.fill_color = 'blue'
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.render()
+    print(rectangle.corner_1)
+    print(rectangle.corner_1.x)
+    print(rectangle.corner_1.y)
+    print(rectangle.corner_2)
+    print(rectangle.corner_2.x)
+    print(rectangle.corner_2.y)
+    print(rectangle.outline_thickness)
+    print(rectangle.fill_color)
+    print(circle.center)
+    print(circle.center.x)
+    print(circle.center.y)
+    print(circle.outline_thickness)
+    print(circle.fill_color)
+    window.close_on_mouse_click()
 def lines():
     """
     -- Constructs a rg.RoseWindow.
@@ -99,7 +130,18 @@ def lines():
 
     -- Waits for the user to press the mouse, then closes the window.
     """
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
+    window = rg.RoseWindow()
+    line1 = rg.Line(rg.Point(100,100),rg.Point(200,200))
+    line2 = rg.Line(rg.Point(300,200),rg.Point(150,250))
+    line2.thickness = 5
+    line1.attach_to(window)
+    line2.attach_to(window)
+    window.render()
+    print(line2.get_midpoint())
+    print(line2.get_midpoint().x)
+    print(line2.get_midpoint().y)
+    window.close_on_mouse_click()
 
 
 # -----------------------------------------------------------------------------
